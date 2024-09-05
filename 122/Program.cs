e263da91183b884784a4d7b98fd8026a3c581195
Student406-11 <Student406-11@NPK-102O-4>
2024-09-05 09:44:11 +0700

    Добавьте файлы проекта.

---
+++ 122/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        Process[] processes = Process.GetProcesses();
+
+        foreach (Process process in processes)
+        {
+            Console.Write("Имя: " + process.ProcessName + ", ID: " + process.Id + ", RAM: " + process.PagedMemorySize64 / 8 / 1024 / 1024 + "MB ");
+            if (process.ProcessName.Length == 0)
+                Console.WriteLine("Status: Stopped");
+            else
+                Console.WriteLine("Status: Running");
+
+        }
+        int ProcessIDin = Convert.ToInt32(Console.ReadLine());
+        Process ProcessId = Process.GetProcessById(ProcessIDin);
+            ProcessId.Kill();
+
+    }
+}
+
